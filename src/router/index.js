0a4063f3e00f371be1ca5d345a4646ca01dce1a1
borcesmileski <borche.smileski@students.finki.ukim.mk>
2023-07-30 16:33:45 +0200

    route change

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Index from '@/pages/Index.vue'
-import Wine from "../pages/Wine.vue";
 import DefaultLayout from "../layouts/DefaultLayout.vue";
+import Winery from "../pages/Winery.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,9 +14,9 @@ const router = createRouter({
           layout:DefaultLayout
         }
     },{
-      path: '/wine',
-      name: 'Wine',
-      component: Wine,
+      path: '/winery',
+      name: 'Winery',
+      component: Winery,
         meta:{
         }
     }
