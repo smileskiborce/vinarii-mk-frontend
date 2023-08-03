15489dde6151dfbadd593d82c306c4b692e68ab0
MartinaDonceva <doncevamartina@yahoo.com>
2023-08-03 18:07:44 +0200

    Footer fixed.

--- src/Components/layouts/CustomFooter.vue
+++ src/Components/layouts/CustomFooter.vue
@@ -4,14 +4,16 @@
       <h4 class="fw-lighter text-center">CONNECT WITH US</h4>
       <hr class="mb-4">
       <div class="d-flex flex-row justify-content-between px-4 mb-5">
-        <a v-for="(media, i) in socialMedia" :key="i" :href=media.link>
+        <a v-for="(media, i) in socialMedia" :key="i" :href="media.link">
           <i class="fa text-white-50 fa-2x" :class="media.fa" aria-hidden="true"></i>
         </a>
       </div>
-      <div class="container">
-        <div class="fs-5 d-flex flex-row justify-content-between" v-for="(link, i) in menuItems" :key="i">
-          <a class="text-decoration-none text-white-50" :href=link.link>{{link.item}}</a>
-        </div>
+
+      <!-- Second Row: Menu Items Links -->
+      <div class="d-flex flex-row justify-content-between px-4 mb-5 fs-4 gap-3">
+        <a v-for="(link, i) in menuItems" :key="i" :href="link.link" class="link-item text-decoration-none text-white-50 d-inline-block">
+          {{link.item}}
+        </a>
       </div>
     </div>
   </div>
