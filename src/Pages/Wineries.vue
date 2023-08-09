439da5519df45fc8acf56580f6b1caef814674e8
MartinaDonceva <doncevamartina@yahoo.com>
2023-08-09 19:14:41 +0200

    Wineries page created.

---
+++ src/Pages/Wineries.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <div class="text-center text-secondary">
+      <h1>WINERIES</h1>
+    </div>
+
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Wineries"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Index from "../Pages/Index.vue";
 import DefaultLayout from "../Layouts/DefaultLayout.vue";
+import Wineries from "@/Pages/Wineries.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -11,6 +12,14 @@ const router = createRouter({
         meta:{
           layout:DefaultLayout
         }
+    },
+    {
+      path: '/wineries',
+      name: 'Wineries',
+      component: Wineries,
+      meta:{
+        layout:DefaultLayout
+      }
     },
   ]
 })
