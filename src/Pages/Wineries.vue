823b847a01547e1d59a0e0d4f9050e81e0ee6b83
MartinaDonceva <doncevamartina@yahoo.com>
2023-08-09 20:58:47 +0200

    Wineries page finished.

--- src/Pages/Wineries.vue
+++ src/Pages/Wineries.vue
@@ -1,15 +1,20 @@
 <template>
-  <main>
-    <div class="text-center text-secondary">
+  <main class="container py-5">
+    <div class="text-center text-secondary mb-5">
       <h1>WINERIES</h1>
+    </div>
+    <div class="row d-flex flex-column justify-content-center">
+      <winery-card/>
     </div>
 
   </main>
 </template>
 
 <script>
+import WineryCard from "@/Components/WineryCard.vue";
 export default {
-  name: "Wineries"
+  name: "Wineries",
+  components: {WineryCard}
 }
 </script>
 
