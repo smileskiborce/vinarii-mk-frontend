7ffc65dcec9eb3fcf275dd9cf2e47ba1a448eb22
borcesmileski <borche.smileski@students.finki.ukim.mk>
2023-08-09 18:30:16 +0200

    cleaned project

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,16 +1,6 @@
 <template>
-  <main class="bg-primary">
-    <hero-section/>
-    <what-do-we-offer-section/>
-    <h2 class="mt-5 fw-lighter text-center text-white-50">WINERIES</h2>
-    <hr class="mb-5 text-white"/>
-    <wineries-section class="d-flex flex-column  align-items-center"/>
-    <div class="text-center my-5">
-      <a class="btn btn-outline-light mb-5 w-25 fs-5 text-white bg-secondary" href="/wineries">See all >></a>
-    </div>
-    <most-popular-wines/>
-    <about-us-section/>
-    <custom-footer/>
+  <main>
+
   </main>
 </template>
 
@@ -22,19 +12,8 @@ export default {
 </script>
 <script setup>
 
-import HeroSection from "../Components/index/HeroSection.vue";
-import CustomFooter from "../Components/layouts/CustomFooter.vue";
-import WhatDoWeOfferSection from "../Components/index/WhatDoWeOfferSection.vue";
-import WineriesSection from "../Components/WineriesSection.vue";
-import AboutUsSection from "@/Components/index/AboutUsSection.vue";
-import MostPopularWines from "@/Components/Wine/MostPopularWines.vue";
-
-
 
 </script>
 
 <style scoped>
-.margin-100px {
-  margin-bottom: 100px;
-}
 </style>
