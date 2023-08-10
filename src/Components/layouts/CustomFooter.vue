542c3e2343f981206aedf89c3b139c3928132e72
MartinaDonceva <doncevamartina@yahoo.com>
2023-08-10 14:28:22 +0200

    Footer design changed and header menu items added.

--- src/Components/layouts/CustomFooter.vue
+++ src/Components/layouts/CustomFooter.vue
@@ -1,19 +1,42 @@
 <template>
-  <div class="bg-primary py-5 text-white d-flex flex-column align-items-center">
-    <div>
-      <h4 class="fw-bold text-center pb-2">CONNECT WITH US</h4>
-      <hr class="mb-4">
-      <div class="d-flex flex-row justify-content-between px-4 mb-4">
-        <a v-for="(media, i) in socialMedia" :key="i" :href="media.link">
-          <i class="fa text-white fa-2x" :class="media.fa" aria-hidden="true"></i>
-        </a>
+  <div class="bg-primary pt-5 text-white">
+    <div class="container">
+      <div class="row mb-4">
+        <div class="col-md-4 d-flex flex-column align-items-center align-items-md-start mb-4 mb-md-0">
+          <h4 class="fw-lighter mb-3">CONTACT US</h4>
+          <div class="d-flex gap-3 mb-2 align-items-center" v-for="(contact, i) in contactInfo" :key="i">
+            <i :class=contact.icon aria-hidden="true"></i>
+            <span>{{ contact.text }}</span>
+          </div>
+        </div>
+        <div class="col-md-4 d-flex flex-column align-items-center mb-4 mb-md-0">
+          <h4 class="mb-3 mb-md-5 fw-lighter">FOLLOW US</h4>
+          <div class="d-flex gap-5 mb-4 ">
+            <a v-for="(media, i) in socialMedia" :key="i" :href="media.link">
+              <i class="fa text-white fa-2x" :class="media.fa" aria-hidden="true"></i>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4 d-flex flex-column align-items-center align-items-md-end mb-3 mb-md-0">
+          <h4 class="fw-lighter mb-3">SUBSCRIBE</h4>
+          <p class="fw-lighter">Always stay in touch and receive news first.</p>
+          <div class="form-floating w-75  text-white mb-2">
+            <input type="email" class="form-control" id="Email" placeholder="Enter your email..." style="height: 10px;">
+            <label for="Email">Email address</label>
+          </div>
+          <button class="btn btn-outline-light w-75">SUBSCRIBE</button>
+        </div>
       </div>
-      <div class="d-flex flex-row justify-content-between px-4 fs-4 gap-5">
-        <a v-for="(link, i) in menuItems" :key="i" :href="link.link" class="link-item text-decoration-none text-white d-inline-block">
-          {{link.item}}
-        </a>
+      <div class="container">
+        <hr/>
+        <div class="row pb-3">
+          <div class="d-flex flex-column flex-md-row justify-content-center align-items-center gap-2 gap-md-5 fs-6">
+            <a v-for="(link, i) in menuItems" :key="i" :href="link.link" class="link-item text-decoration-none text-white">
+              {{link.item}}
+            </a>
+          </div>
+        </div>
       </div>
-
     </div>
   </div>
 </template>
@@ -40,15 +63,39 @@ const socialMedia = [
 ]
 const menuItems = [
   {
+    item: 'HOME',
+    link: '/'
+  },
+  {
     item: 'WINERIES',
     link: '/wineries'
   },
   {
     item: 'ABOUT US',
     link: '#'
+  },
+  {
+    item: 'CONTACT',
+    link: '#'
+  },
+]
+const contactInfo = [
+  {
+    icon: 'fa fa-map-marker',
+    text: 'ul. Kocho Racin br. 70, Skopje'
+  },
+  {
+    icon: 'fa fa-envelope',
+    text: 'name@example.com'
+  },
+  {
+    icon: 'fa fa-phone',
+    text: '+38978999000'
   }
 ]
 </script>
 <style scoped>
-
+.form-floating .form-control {
+  height: 10px;
+}
 </style>
--- src/Components/layouts/CustomHeader.vue
+++ src/Components/layouts/CustomHeader.vue
@@ -1,6 +1,6 @@
 <template>
 <section>
-  <nav class="navbar navbar-expand-lg py-3 " >
+  <nav class="navbar navbar-expand-lg py-3" >
     <div class="container">
       <a class="navbar-brand" href="/"><img src="../../assets/images/logo.svg"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,8 +8,9 @@
       </button>
       <div class="collapse navbar-collapse " id="navbar">
         <div class="navbar-nav ms-lg-auto text-center fs-5 nav-item" >
-          <a class="nav-link text-dark" aria-current="page" href="#wineries-section">WINERIES</a>
-          <a class="nav-link text-dark" aria-current="page" href="#about-us">ABOUT US</a>
+          <a class="nav-link text-dark" aria-current="page" href="/wineries">WINERIES</a>
+          <a class="nav-link text-dark" aria-current="page" href="#">ABOUT US</a>
+          <a class="nav-link text-dark" aria-current="page" href="#">CONTACT</a>
         </div>
       </div>
     </div>
