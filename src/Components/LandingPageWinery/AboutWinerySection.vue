7be910e57b934b2f08749db9661222c50bc16f08
Natali <natalivarga25@hotmail.com>
2023-08-11 08:38:32 +0200

    Winery Landing Page Done

---
+++ src/Components/LandingPageWinery/AboutWinerySection.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card my-3">
+    <div class="card-header">
+      About the winery
+    </div>
+    <div class="card-body">
+      <div class="text-center">DESCRIPTION</div>
+      <hr/>
+      <div class="fa fa-globe mb-3 d-block">
+        <a class="px-2" href="#">page-of-winery.com</a>
+      </div>
+      <div class="fa fa-envelope my-3 d-block">
+        <span class="px-2">winery_email@gmail.com</span>
+      </div>
+      <div class="fa fa-address-book my-3 d-block">
+        <span class="px-2">+38977777777</span>
+      </div>
+      <div class="fa fa-clock-o mt-3 d-block">
+        <span class="px-2">Mon-Fri: 08:00-16:00</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
