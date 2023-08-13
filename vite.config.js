c9d5f9341e4d1504c2054cfaf864a644eda7cc00
borcesmileski <borche.smileski@students.finki.ukim.mk>
2023-08-13 14:28:09 +0200

    tring to fix routes

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': `${__dirname}/src` // Use the computed __dirname variable
     }
   }
 })
