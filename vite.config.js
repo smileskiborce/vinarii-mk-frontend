025dade35c404b0ee7e8e26eccf959e0fe428647
borcesmileski <borche.smileski@students.finki.ukim.mk>
2023-08-13 14:31:18 +0200

    reseting configs

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': `${__dirname}/src` // Use the computed __dirname variable
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
