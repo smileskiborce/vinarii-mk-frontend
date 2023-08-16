430f2207094ebf7fdfe3e4bdbfbe298c645d8203
Natali <natalivarga25@hotmail.com>
2023-08-16 09:17:34 +0200

    Added Multi language function

---
+++ src/Components/LanguageSwitcher.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script setup>
+</script>
+
+<script>
+
+import {useI18n} from "vue-i18n";
+
+export default {
+  setup() {
+    const { t, locale } = useI18n()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import AboutUs from "../Pages/About-us.vue";
 import ContactUs from "../Pages/ContactUs.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(import.meta.env.VITE_BASE_URL),
     routes: [
         {
             path: '/',
